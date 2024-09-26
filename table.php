c6cd65f5987e468d195a930540b9326443cf5bac
rostikcheremys <rostikcheremys@gmail.com>
2024-09-26 23:08:45 +0300

    Initial commit

---
+++ table.php
@@ -0,0 +1,44 @@
+<?php
+
+$filename = 'data/oblinfo.txt';
+
+if (file_exists($filename)) {
+    $file = fopen($filename, 'r');
+
+    $data = [];
+
+    while (($line = fgets($file)) !== false) {
+
+        $line = trim($line);
+
+        $data[] = $line;
+    }
+
+    fclose($file);
+
+    echo "<table border='1' cellpadding='10' cellspacing='0'>";
+    echo "<tr><th>№</th><th>Область</th><th>Населення (тис.)</th><th>Кількість вищих навчальних закладів</th><th>Кількість вищих навчальних закладів на 100 тис. населення</th></tr>";
+
+    $number = 0;
+
+    for ($i = 0; $i < count($data); $i += 3) {
+
+        $number++;
+        $population = $data[$i + 1];
+        $universities = $data[$i + 2];
+        $universitiesPer100k = ($population != 0) ? round($universities * 100 / $population, 2) : 0;
+
+        echo "<tr>";
+        echo "<td>{$number}</td>";
+        echo "<td>{$data[$i]}</td>";
+        echo "<td>{$data[$i + 1]}</td>";
+        echo "<td>{$data[$i + 2]}</td>";
+        echo "<td>{$universitiesPer100k}</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+} else {
+    echo "Файл не знайдено!";
+}
+
